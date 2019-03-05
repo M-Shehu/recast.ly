dac0c9c5392ae90654ab37e21289d5f57ad9f149
M-Shehu <magnanimus.shehu@gmail.com>
2019-03-04 19:53:40 -0800

    Implement First 3 Tests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,32 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js'
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  {/* {console.log(props)}; */}
+    {props.videos.map(video => 
+    // {console.log(video.snippet.title)}
+      <VideoListEntry title={video.snippet.title} description={video.snippet.description} imageUrl={video.snippet.thumbnails.default.url}/>
+    )}
   </div>
 );
+
+// class VideoList extends React.Component {
+//   constructor(props) {
+//     super(props)
+
+//   }
+
+//   render() {
+//     return (
+//       <div className="video-list">
+//         {props.videos.map(video => 
+//           <VideoListEntry video = {video} />
+//         )}
+//       </div>
+//     )
+//   }
+  
+// }
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
